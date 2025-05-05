cdabcd65e5ea5d07077fa3dcdd96378ac27d0636
ex-jiangpengpeng001 <jiangpengpengv@126.com>
2025-05-06 00:36:59 +0800

    internal-common v1.0

--- service-verificationcode/src/main/java/com/mashibing/serviceverificationcode/controller/NumberCodeController.java
+++ service-verificationcode/src/main/java/com/mashibing/serviceverificationcode/controller/NumberCodeController.java
@@ -1,6 +1,8 @@
 package com.mashibing.serviceverificationcode.controller;
 
 import com.alibaba.fastjson.JSONObject;
+import com.mashibing.internalcommon.dto.ResponseResult;
+import com.mashibing.internalcommon.responese.NumberCodeResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class NumberCodeController {
 
     @GetMapping("/numberCode/{size}")
-    public String numberCode(@PathVariable("size") int size) {
+    public ResponseResult numberCode(@PathVariable("size") int size) {
 
         System.out.println("size:"+size);
         // 生成验证码
@@ -18,13 +20,10 @@ public class NumberCodeController {
         int resultInt = (int)mathRandom;
         System.out.println("generator src code:" + resultInt);
 
-        JSONObject result = new JSONObject();
-        result.put("code", 1);
-        result.put("message", "success");
-        JSONObject data = new JSONObject();
-        data.put("numberCode", resultInt);
-        result.put("data", data);
+        // 定义返回值
+        NumberCodeResponse response = new NumberCodeResponse();
+        response.setNumberCode(resultInt);
 
-        return result.toString();
+        return ResponseResult.success(response);
     }
 }
