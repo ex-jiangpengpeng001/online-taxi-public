c5785dae8f23f7ab4e2352c023a1591eb7559a88
ex-jiangpengpeng001 <jiangpengpengv@126.com>
2025-05-06 22:59:44 +0800

    api-passenger v1.1.3

--- api-passenger/src/main/java/com/mashibing/apipassenger/service/VerificationCodeService.java
+++ api-passenger/src/main/java/com/mashibing/apipassenger/service/VerificationCodeService.java
@@ -1,15 +1,24 @@
 package com.mashibing.apipassenger.service;
 
 import com.alibaba.fastjson.JSONObject;
+import com.mashibing.apipassenger.remote.ServiceVefificationcodeClient;
+import com.mashibing.internalcommon.dto.ResponseResult;
+import com.mashibing.internalcommon.responese.NumberCodeResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class VerificationCodeService {
 
+    @Autowired
+    private ServiceVefificationcodeClient serviceVefificationcodeClient;
+
     public String generatorCode(String passengerPhone) {
         // 调用验证码服务，获取验证码
-        System.out.println("调用验证码服务，获取验证码");
-        String code = "111111";
+        ResponseResult<NumberCodeResponse> numberCodeResponse = serviceVefificationcodeClient.getNumberCode(6);
+        int numberCode = numberCodeResponse.getData().getNumberCode();
+
+        System.out.println("remote number code:" + numberCode);
 
         // 存入redis
         System.out.println("存入redis");
