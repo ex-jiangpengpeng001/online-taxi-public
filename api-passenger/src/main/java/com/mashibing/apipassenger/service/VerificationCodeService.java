607bc48ddc4c95228af22ad176afe4f931b20be8
ex-jiangpengpeng001 <jiangpengpengv@126.com>
2025-06-07 10:59:55 +0800

    api-passenger v1.1.6

--- api-passenger/src/main/java/com/mashibing/apipassenger/service/VerificationCodeService.java
+++ api-passenger/src/main/java/com/mashibing/apipassenger/service/VerificationCodeService.java
@@ -1,6 +1,8 @@
 package com.mashibing.apipassenger.service;
 
+import com.alibaba.cloud.commons.lang.StringUtils;
 import com.mashibing.apipassenger.remote.ServiceVefificationcodeClient;
+import com.mashibing.internalcommon.constant.CommonStatusEnum;
 import com.mashibing.internalcommon.dto.ResponseResult;
 import com.mashibing.internalcommon.responese.NumberCodeResponse;
 import com.mashibing.internalcommon.responese.TokenResponse;
@@ -33,12 +35,21 @@ public class VerificationCodeService {
         int numberCode = numberCodeResponse.getData().getNumberCode();
 
         // key,value,过期时间
-        String key = verificationCodePrefix + passengerPhone;
+        String key = generatorKeyByPhone(passengerPhone);
         // 存入redis
         stringRedisTemplate.opsForValue().set(key, numberCode+"", 2, TimeUnit.MINUTES);
 
         // 通过短信服务商，将对应的验证码发送到手机上。阿里短信服务，腾讯短信通，华信，容联
         return ResponseResult.success("");
+    }
+
+    /**
+     * 根据手机号，生成key
+     * @param passengerPhone
+     * @return
+     */
+    private String generatorKeyByPhone(String passengerPhone) {
+        return verificationCodePrefix + passengerPhone;
     }
 
     /**
@@ -49,8 +60,16 @@ public class VerificationCodeService {
      */
     public ResponseResult checkCode(String passengerPhone , String verificationCode){
         // 根据手机号，去redis读取验证码
+        // 生成key
+        String key = generatorKeyByPhone(passengerPhone);
+        // 根据key获取value
+        String codeRedis = stringRedisTemplate.opsForValue().get(key);
+        System.out.println("redis中的value："+codeRedis);
 
         // 校验验证码
+        if (StringUtils.isBlank(codeRedis)) {
+            return ResponseResult.fail(CommonStatusEnum.VERIFICATION_CODE_ERROR.getCode(), CommonStatusEnum.VERIFICATION_CODE_ERROR.getValue());
+        }
 
         // 判断原来是否有用户，并进行对应的处理
 
