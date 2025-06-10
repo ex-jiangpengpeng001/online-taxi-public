1f318e6c8e8d386cc6ad6a74cf20729020a453fc
ex-jiangpengpeng001 <jiangpengpengv@126.com>
2025-06-11 00:05:12 +0800

    internal-common v1.0.2

--- internal-common/src/main/java/com/mashibing/internalcommon/util/JwtUtils.java
+++ internal-common/src/main/java/com/mashibing/internalcommon/util/JwtUtils.java
@@ -3,6 +3,8 @@ package com.mashibing.internalcommon.util;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTCreator;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.Claim;
+import com.auth0.jwt.interfaces.DecodedJWT;
 
 import java.util.Calendar;
 import java.util.Date;
@@ -14,8 +16,13 @@ public class JwtUtils {
     // 盐
     private static final String SIGN = "CPFmsb!@#$$";
 
+    private static final String JWT_KEY = "passengerPhone";
+
     // 生成token
-    public static String generatorToken(Map<String, String> map) {
+    public static String generatorToken(String passengerPhone) {
+        Map<String,String> map = new HashMap<>();
+        map.put(JWT_KEY, passengerPhone);
+
         // tokeng过期时间
         Calendar calendar = Calendar.getInstance();
         calendar.add(Calendar.DATE, 1);
@@ -34,12 +41,19 @@ public class JwtUtils {
         return sign;
     }
 
+    // 解析token
+    public static String parseToken(String token) {
+        DecodedJWT verify = JWT.require(Algorithm.HMAC256(SIGN)).build().verify(token);
+        Claim claim = verify.getClaim(JWT_KEY);
+        return claim.toString();
+
+    }
+
     public static void main(String[] args) {
-        Map<String, String> map = new HashMap<>();
-        map.put("name", "zhang san");
-        map.put("age", "18");
-        String s = generatorToken(map);
+        String s = generatorToken("13910733521");
         System.out.println("生成的token:" + s);
+
+        System.out.println("解析token后的值：" + parseToken(s));
     }
 
 }
