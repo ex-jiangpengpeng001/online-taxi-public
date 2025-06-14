b8beb475fc24f94acd0456904987593f16cc4cfc
ex-jiangpengpeng001 <jiangpengpengv@126.com>
2025-06-14 23:00:56 +0800

    api-passenger v1.1.9.2

--- api-passenger/src/main/java/com/mashibing/apipassenger/interceptor/JwtInterceptor.java
+++ api-passenger/src/main/java/com/mashibing/apipassenger/interceptor/JwtInterceptor.java
@@ -31,25 +31,10 @@ public class JwtInterceptor implements HandlerInterceptor {
 
         String token = request.getHeader("Authorization");
         // 解析token
-        TokenResult tokenResult = null;
-        try {
-            tokenResult = JwtUtils.parseToken(token);
-        } catch (SignatureVerificationException e) {
-            resutltString = "token sign error";
-            result = false;
-        } catch (TokenExpiredException e) {
-            resutltString = "token time out";
-            result = false;
-        } catch (AlgorithmMismatchException e) {
-            resutltString = "token AlgorithmMismatchException";
-            result = false;
-        } catch (Exception e) {
-            resutltString = "token invalid";
-            result = false;
-        }
+        TokenResult tokenResult = JwtUtils.checkToken(token);
 
         if (tokenResult == null) {
-            resutltString = "token invalid";
+            resutltString = "access token invalid";
             result = false;
         } else {
             // 拼接key
@@ -59,16 +44,10 @@ public class JwtInterceptor implements HandlerInterceptor {
             String tokenKey = RedisPrefixUtils.generatorTokenKey(phone, identity, TokenConstants.ACCESS_TOKEN_TYPE);
             // 从redis中取出token
             String tokenRedis = stringRedisTemplate.opsForValue().get(tokenKey);
-            if (StringUtils.isBlank(tokenRedis)) {
-                resutltString = "token invalid";
+            if ((StringUtils.isBlank(tokenRedis))  || (!token.trim().equals(tokenRedis.trim()))){
+                resutltString = "access token invalid";
                 result = false;
-            } else {
-                if (!token.trim().equals(tokenRedis.trim())) {
-                    resutltString = "token invalid";
-                    result = false;
-                }
             }
-
         }
 
         if (!result) {
