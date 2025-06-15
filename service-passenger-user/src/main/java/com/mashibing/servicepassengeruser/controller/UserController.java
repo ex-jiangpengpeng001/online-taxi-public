b1f539c4c47abb0a27716acc3b9615e250821ed9
ex-jiangpengpeng001 <jiangpengpengv@126.com>
2025-06-15 18:18:44 +0800

    service-passenger-user v1.0.4.0

--- service-passenger-user/src/main/java/com/mashibing/servicepassengeruser/controller/UserController.java
+++ service-passenger-user/src/main/java/com/mashibing/servicepassengeruser/controller/UserController.java
@@ -4,9 +4,7 @@ import com.mashibing.internalcommon.dto.ResponseResult;
 import com.mashibing.internalcommon.request.VerificationCodeDTO;
 import com.mashibing.servicepassengeruser.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UserController {
@@ -22,4 +20,9 @@ public class UserController {
         return userService.loginOrRegister(passengerPhone);
     }
 
+    @GetMapping("/user/")
+    public ResponseResult getUser(@RequestBody VerificationCodeDTO verificationCodeDTO){
+        String passengerPhone = verificationCodeDTO.getPassengerPhone();
+        return userService.getUserByPhone(passengerPhone);
+    }
 }
