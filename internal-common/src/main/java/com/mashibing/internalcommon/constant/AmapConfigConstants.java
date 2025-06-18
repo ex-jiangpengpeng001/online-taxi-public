104388f192e93a6ac1d6e101395707f699760698
ex-jiangpengpeng001 <jiangpengpengv@126.com>
2025-06-18 23:25:57 +0800

    serice-map v1.0.0.2

---
+++ internal-common/src/main/java/com/mashibing/internalcommon/constant/AmapConfigConstants.java
@@ -0,0 +1,10 @@
+package com.mashibing.internalcommon.constant;
+
+public class AmapConfigConstants {
+
+    /**
+     * 路径规划地址
+     */
+    public static final String DIRECTION_URL = "https://restapi.amap.com/v3/direction/driving";
+
+}
--- serice-map/src/main/java/com/mashibing/servicemap/service/DirectionService.java
+++ serice-map/src/main/java/com/mashibing/servicemap/service/DirectionService.java
@@ -2,12 +2,20 @@ package com.mashibing.servicemap.service;
 
 import com.mashibing.internalcommon.dto.ResponseResult;
 import com.mashibing.internalcommon.responese.DirectionResponse;
+import com.mashibing.servicemap.remote.MapDirectionClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DirectionService {
 
+    @Autowired
+    private MapDirectionClient mapDirectionClient;
+
     public ResponseResult driving(String depLongitude,String depLatitude, String destLongitude,String destLatitude) {
+
+        // 调用第三方地图接口
+        mapDirectionClient.direction(depLongitude, depLatitude, destLongitude, destLatitude);
 
         DirectionResponse direction = new DirectionResponse();
         direction.setDistance(123);
