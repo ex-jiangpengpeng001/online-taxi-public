87d530e004d4f4e18c9cd558122ec99a4617e8dd
ex-jiangpengpeng001 <jiangpengpengv@126.com>
2025-06-26 23:10:50 +0800

    service-driver-user 1.0.0.4

---
+++ service-driver-user/src/main/java/com/mashibing/serviceDriverUser/mapper/CarMapper.java
@@ -0,0 +1,18 @@
+package com.mashibing.serviceDriverUser.mapper;
+
+import com.mashibing.internalcommon.dto.Car;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.springframework.stereotype.Repository;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author JPP
+ * @since 2025-06-26
+ */
+@Repository
+public interface CarMapper extends BaseMapper<Car> {
+
+}
