e654a2d956fc1b63dd01670413c1576c83751ed4
ex-jiangpengpeng001 <jiangpengpengv@126.com>
2025-07-07 21:18:44 +0800

    service-driver-user 1.0.0.8

--- service-driver-user/src/main/java/com/mashibing/serviceDriverUser/service/CarService.java
+++ service-driver-user/src/main/java/com/mashibing/serviceDriverUser/service/CarService.java
@@ -2,7 +2,9 @@ package com.mashibing.serviceDriverUser.service;
 
 import com.mashibing.internalcommon.dto.Car;
 import com.mashibing.internalcommon.dto.ResponseResult;
+import com.mashibing.internalcommon.responese.TerminalResponse;
 import com.mashibing.serviceDriverUser.mapper.CarMapper;
+import com.mashibing.serviceDriverUser.remote.ServiceMapClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,6 +12,9 @@ import java.time.LocalDateTime;
 
 @Service
 public class CarService {
+
+    @Autowired
+    private ServiceMapClient serviceMapClient;
 
     @Autowired
     private CarMapper carMapper;
@@ -20,6 +25,13 @@ public class CarService {
         car.setGmtCreate(now);
         // 保存车辆
         carMapper.insert(car);
+
+        // 获得此车辆的终端id：tid
+        ResponseResult<TerminalResponse> responseResult = serviceMapClient.addTerminal(car.getVehicleNo(), car.getId()+"");
+        String tid = responseResult.getData().getTid();
+        car.setTid(tid);
+
+        carMapper.updateById(car);
 
         return ResponseResult.success("");
     }
