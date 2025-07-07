b5050c2d5d0f93b92d960c4a6380d44e87031804
ex-jiangpengpeng001 <jiangpengpengv@126.com>
2025-07-07 22:23:26 +0800

    service-driver-user 1.0.0.9

--- service-driver-user/src/main/java/com/mashibing/serviceDriverUser/service/CarService.java
+++ service-driver-user/src/main/java/com/mashibing/serviceDriverUser/service/CarService.java
@@ -3,6 +3,7 @@ package com.mashibing.serviceDriverUser.service;
 import com.mashibing.internalcommon.dto.Car;
 import com.mashibing.internalcommon.dto.ResponseResult;
 import com.mashibing.internalcommon.responese.TerminalResponse;
+import com.mashibing.internalcommon.responese.TrackResponse;
 import com.mashibing.serviceDriverUser.mapper.CarMapper;
 import com.mashibing.serviceDriverUser.remote.ServiceMapClient;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,6 +31,14 @@ public class CarService {
         ResponseResult<TerminalResponse> responseResult = serviceMapClient.addTerminal(car.getVehicleNo(), car.getId()+"");
         String tid = responseResult.getData().getTid();
         car.setTid(tid);
+
+        // 获得此车辆的轨迹id：trid
+        ResponseResult<TrackResponse> trackResponseResponseResult = serviceMapClient.addTrack(tid);
+        String trid = trackResponseResponseResult.getData().getTrid();
+        String trname = trackResponseResponseResult.getData().getTrname();
+
+        car.setTrid(trid);
+        car.setTrname(trname);
 
         carMapper.updateById(car);
 
