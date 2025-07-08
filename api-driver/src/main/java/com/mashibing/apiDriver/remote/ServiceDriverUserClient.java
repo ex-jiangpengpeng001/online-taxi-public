4f8c1cfd2c61afe1f60c3d02f9cba659dd89491a
ex-jiangpengpeng001 <jiangpengpengv@126.com>
2025-07-08 22:50:23 +0800

    api-driver 1.0.0.6

--- api-driver/src/main/java/com/mashibing/apiDriver/remote/ServiceDriverUserClient.java
+++ api-driver/src/main/java/com/mashibing/apiDriver/remote/ServiceDriverUserClient.java
@@ -1,13 +1,11 @@
 package com.mashibing.apiDriver.remote;
 
+import com.mashibing.internalcommon.dto.Car;
 import com.mashibing.internalcommon.dto.DriverUser;
 import com.mashibing.internalcommon.dto.ResponseResult;
 import com.mashibing.internalcommon.responese.DriverUserExistsResponse;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 @FeignClient("service-driver-user")
 public interface ServiceDriverUserClient {
@@ -18,4 +16,6 @@ public interface ServiceDriverUserClient {
     @RequestMapping(method = RequestMethod.GET, value = "/check-driver/{driverPhone}")
     public ResponseResult<DriverUserExistsResponse> checkDriver(@PathVariable("driverPhone") String driverPhone);
 
+    @RequestMapping(method = RequestMethod.GET, value = "/car")
+    public ResponseResult<Car> getCarById(@RequestParam Long carId);
 }
--- service-driver-user/src/main/java/com/mashibing/serviceDriverUser/controller/CarController.java
+++ service-driver-user/src/main/java/com/mashibing/serviceDriverUser/controller/CarController.java
@@ -4,6 +4,7 @@ import com.mashibing.internalcommon.dto.Car;
 import com.mashibing.internalcommon.dto.ResponseResult;
 import com.mashibing.serviceDriverUser.service.CarService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,5 +26,11 @@ public class CarController {
     @PostMapping("/car")
     public ResponseResult addCar(@RequestBody Car car){
         return carService.addCar(car);
+    }
+
+    @GetMapping("/car")
+    public ResponseResult<Car> getCarById(Long carId){
+
+        return carService.getCarById(carId);
     }
 }
