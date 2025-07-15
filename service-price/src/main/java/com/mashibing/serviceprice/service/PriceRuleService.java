d6085589f34ac4bf0a16647481847387ae415772
ex-jiangpengpeng001 <jiangpengpengv@126.com>
2025-07-15 23:25:39 +0800

    service-price v1.0.0.6

--- service-price/src/main/java/com/mashibing/serviceprice/service/PriceRuleService.java
+++ service-price/src/main/java/com/mashibing/serviceprice/service/PriceRuleService.java
@@ -51,4 +51,45 @@ public class PriceRuleService {
         return ResponseResult.success("");
     }
 
+    public ResponseResult edit(PriceRule priceRule){
+        // 拼接fareType
+        String cityCode = priceRule.getCityCode();
+        String vehicleType = priceRule.getVehicleType();
+        String fareType = cityCode + "$" + vehicleType;
+        priceRule.setFareType(fareType);
+
+        // 添加版本号
+        // 问题1：增加了版本号，前面的两个字段无法唯一确定一条记录，问题2：找最大的版本号，需要排序
+
+        QueryWrapper<PriceRule> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("city_code",cityCode);
+        queryWrapper.eq("vehicle_type",vehicleType);
+        queryWrapper.orderByDesc("fare_version");
+
+        List<PriceRule> priceRules = priceRuleMapper.selectList(queryWrapper);
+        Integer fareVersion = 0;
+        if (priceRules.size()>0){
+            PriceRule lasterPriceRule = priceRules.get(0);
+            Double unitPricePerMile = lasterPriceRule.getUnitPricePerMile();
+            Double unitPricePerMinute = lasterPriceRule.getUnitPricePerMinute();
+            Double startFare = lasterPriceRule.getStartFare();
+            Integer startMile = lasterPriceRule.getStartMile();
+
+            if (unitPricePerMile.doubleValue() == priceRule.getUnitPricePerMile().doubleValue()
+                    && unitPricePerMinute.doubleValue() == priceRule.getUnitPricePerMinute().doubleValue()
+                    && startFare.doubleValue() == priceRule.getStartFare().doubleValue()
+                    && startMile.intValue() == priceRule.getStartMile().intValue()){
+                return ResponseResult.fail(CommonStatusEnum.PRICE_RULE_NOT_EDIT.getCode(),CommonStatusEnum.PRICE_RULE_NOT_EDIT.getValue());
+            }
+
+
+            fareVersion = lasterPriceRule.getFareVersion();
+        }
+        priceRule.setFareVersion(++fareVersion);
+
+
+        priceRuleMapper.insert(priceRule);
+        return ResponseResult.success();
+    }
+
 }
